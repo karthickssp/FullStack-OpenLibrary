430449918f2e6ed6471625a56a20e46fb5719207
KARTHICK SSP <110531394+karthickssp@users.noreply.github.com>
2023-02-26 21:58:12 +0530

    Update App.js

--- React-Frontend-OpenLibrary/src/App.js
+++ React-Frontend-OpenLibrary/src/App.js
@@ -2,13 +2,22 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
 import Register from './Components/Register';
-
+import ListBook from './Elements/ListBook';
+import CreateBook from './Elements/CreateBook';
+import UpdateBook from './Elements/UpdateBook';
+import ViewBook from './Elements/ViewBook';
 function App() {
   return (
     <div className="App">
       <Route>
         <BrowserRouter>
           <switch>
+ -            <Route path = "/" exact component = {Login}></Route>
+              <Route path = "/register" component = {Register}></Route>
+              <Route path = "/books" exact component = {ListBook}></Route>
+              <Route path = "/book" component = {CreateBook}></Route>
+              <Route path = "/add-book/:id" component = {UpdateBook}></Route>
+              <Route path = "/view-book/:id" component = {ViewBook}></Route>
           </switch>
         </BrowserRouter>
       </Route>
